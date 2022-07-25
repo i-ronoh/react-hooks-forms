91593ba0fa2c4e90d2826bc06fdc414a6e151a2c
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-07-26 00:21:49 +0300

    Done.

---
+++ src/components/DisplayData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DisplayData(props){
+    return(
+      <div>
+        <h1>{props.firstName} {props.lastName}</h1>
+      </div>
+    )
+  }
+  export default DisplayData;
+  
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleFirstNameChange (e){
+    setFirstName(e.target.value);
+    console.log(e.target.value)
+  }
+
+  function handleLastNameChange(e){
+    setLastName(e.target.value);
+    console.log(e.target.value)
+  }
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input onChange={handleFirstNameChange} type="text" value={firstName} />
+      <input onChange={handleLastNameChange}  type="text" value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
